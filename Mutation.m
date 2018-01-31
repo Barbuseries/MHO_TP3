362c904538a5efc3426ad57352d7f9555d643bb7
Edorh François <francois.edorh@isen-lille.fr>
2018-01-31 15:10:04 -0400

    Add normal mutation

--- Mutation.m
+++ Mutation.m
@@ -7,6 +7,8 @@ function Mutation
   %% Arithmetic
   MUTATION.uniform = @uniform;
   MUTATION.boundary = @boundary;
+  MUTATION.normal = @normal;
+  MUTATION.normalN = @normalN;
 end
 
 %% TODO: Seems like what is called here 'mask_as_array' and in other
@@ -34,7 +36,6 @@ function result = uniform(children, constraints, mutations)
   global UTILS;
   
   dim = size(children);
-  dim2 = size(constraints);
 
   N = dim(1);
   
@@ -43,14 +44,11 @@ function result = uniform(children, constraints, mutations)
   result = children .* (mutations == 0) + mutations .* UTILS.randomIn(constraints, N);
 end
 
-function result = boundary(children, constraints, mutations)
-  global UTILS;
-  
+function result = boundary(children, constraints, mutations) 
   dim = size(children);
-  dim2 = size(constraints);
 
   N = dim(1);
-  var_count = dim2(1);
+  var_count = dim(2);
   
   %% TODO(@perf): This can probably be improved (I hope).
   %% TODO: Explain!
@@ -61,4 +59,41 @@ function result = boundary(children, constraints, mutations)
   clamp_down = constraints(:, 1)' .* ((mutations > 0) & (mutations <= 0.5));
   
   result = keep + clamp_up + clamp_down;
+end
+
+%% TODO: Should the the sigma be random or set by the user?
+function result = normal(children, constraints, mutations)
+  dim = size(children);
+  
+  N = dim(1);
+  
+  %% TODO: Explain!
+  sigma = rand(N, 1) .* (mutations == 1);
+  result = normalAnyInner_(sigma, children, constraints);
+end
+
+%% TODO: Should the the sigma be random or set by the user?
+%% TODO: This was not run!
+function result = normalN(children, constraints, mutations)
+  dim = size(children);
+  
+  N = dim(1);
+  var_count = dim(2);
+  
+  %% TODO: Explain!
+  sigma = rand(N, var_count) .* (mutations == 1);
+  result = normalAnyInner_(sigma, children, constraints);
+end
+
+%% TODO: Explain!
+function result = normalAnyInner_(sigma, children, constraints)
+  non_zero = (sigma ~= 0);
+  sigma_non_zero = sigma(non_zero);
+  sigma(non_zero) = sigma_non_zero .* normrnd(0, 1, size(sigma_non_zero));
+  
+  lowest = constraints(:, 1)';
+  biggest = constraints(:, 2)';
+  
+  result = children + sigma;
+  result = max(min(result, biggest), lowest);
 end
